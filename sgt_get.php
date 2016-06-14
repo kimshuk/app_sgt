60792039ce9ac2d3204b90d3b21762dc4d112947
Andrew Kim <jeesoo2002@gmail.com>
2016-06-14 10:26:06 -0700

    get data query done

--- sgt_get.php
+++ sgt_get.php
@@ -10,7 +10,18 @@ require_once('mysql_connect.php');
 
 mysqli_select_db("sgt_db") or die("couldn't connect to database");
 
-$query = "SELECT `student id``class id``grade` FROM  
-          WHERE ";
+$query = "SELECT `class`.`name`, `student`.`name`, `grade`.`grade` FROM `grade`
+          JOIN `student`
+          ON `student`.`id` = `grade`.`student_id`
+          JOIN `class`
+          ON `grade`.`class_id` = `class`.`id`";
 
 $result = mysqli_query($conn, $query);
+
+if(mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result)){
+        $new_id = mysqli_insert_id($conn);
+        $output['success'] = true;
+        $output['new_id'] = $new_id;
+    }
+}
