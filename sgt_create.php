e35909fb87a014e7ecf55bb2b924d1d763db5239
Andrew Kim <jeesoo2002@gmail.com>
2016-06-16 11:04:33 -0700

    sgt_add.php works

--- sgt_create.php
+++ sgt_create.php
@@ -7,26 +7,50 @@
  */
 require_once('mysql_connect.php');
 
+//$_POST => {
+//    "student_name" => $student;
+//};
+
+
 $student = $_POST["student_name"];
 $course = $_POST["student_course"];
 $grade = $_POST["student_grade"];
+
 //do validation for duplicate data
+//if(isset($_POST)){
+//    foreach ($_POST as $key => $value){
+//        if($_POST[$key] == ) {
+//
+//        }
+//    }
+//}
 
 
 $query1 = "INSERT INTO `student`.`student_name` VALUES `$student`";
 //do query
 $result = mysqli_query($conn, $query1);
 //get insert id and store in variable
-$id = mysqli_insert_id($conn);
+$student_id = mysqli_insert_id($conn);
 $query2 = "INSERT INTO `class`.`class_name` VALUES `$course`";
 //do query
 $result = mysqli_query($conn, $query2);
 //get insert id and store in different variable
-$
+$class_id = mysqli_insert_id($conn);
+
 $query3 = "INSERT INTO `grade` SET  
             `score` = `$grade`,
-            SELECT `class`.`id` FROM `class`
-            WHERE `class`.`id` = `grade`.`class_id`";
+            `class_id` = `$class_id`,
+            `student_id` = `$student_id`";
 
+$result = mysqli_query($conn, $query3);
+
+
+if(mysqli_num_rows($result) >0) {
+    while($row = mysqli_fetch_assoc($result)){
+        $output = $row;
+    }
+    $response = json_encode($output);
+    print_r($response);
+}
 
 ?>
--- sgt_get.php
+++ sgt_get.php
@@ -18,7 +18,7 @@ $result = mysqli_query($conn, $query);
 
 if(mysqli_num_rows($result) > 0){
     while($row = mysqli_fetch_assoc($result)){
-        $output[] = $row;
+        $output = $row;
     }
     $response = json_encode($output);
     print_r($response);
